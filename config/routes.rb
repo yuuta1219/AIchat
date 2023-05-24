2b81d9cf17ffbd84773c63b9647e7c90c915079d
yuuta1219 <yuuta.sakiyama.1219@gmail.com>
2023-05-24 21:30:19 +0900

    仮ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   root :to => 'users#index'
   resources :users
+  
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
